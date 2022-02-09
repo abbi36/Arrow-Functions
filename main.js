91f50fe9eafd8046c60a3e47e5f756e37868cdff
Abbi Engel <abbi.engel33@gmail.com>
2022-02-09 11:24:18 -0600

    commit 2

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-const names = [
-    'Lisa',
-    'DShane',
-    'Kati',
-    'Parish',
-    'Abbi',
-]
+import got from 'got';
+
+/**
+ * Get a user's repos off of GitHub
+ * @param {string} username 
+ * @param {function} callback This will return an error and an array of repositories
+ * Documentation: https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user
+ */
+async function github(username, callback) {
+    let err, data;
+    try {
+        const resp = got(`https://api.github.com/users/${username}/repos`)
+        data = await resp.json();
+        callback(err, data);
+    } catch (err) {
+        callback(err, data)
+    }
+}
 
 function main() {
-    const sorted = names.sort((a,b) => {
-        if (a < b) {
-            return -1;
-        } else if (a > b) {
-            return 1;
-        }  else { return 0;
+    github('abbi36', (error, projects) => {
+        for (let project of projects){
+            console.log(`${project.name} has ${project.stargazers_count} and it is ${project.description}`);
         }
     });
-    console.log(sorted);
 }
 
 main();
