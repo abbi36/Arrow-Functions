fcdecf62d0a1ca173d47bda3fe7ba123ca1e28fc
Abbi Engel <abbi.engel33@gmail.com>
2022-02-09 10:02:32 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const names = [
+    'Lisa',
+    'DShane',
+    'Kati',
+    'Parish',
+    'Abbi',
+]
+
+function main() {
+    const sorted = names.sort((a,b) => {
+        if (a < b) {
+            return -1;
+        }
+    });
+    console.log(sorted);
+}
+
+main();
