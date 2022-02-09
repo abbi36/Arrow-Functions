64e176b51501f4950eface8335c3877e549b1ff9
Abbi Engel <abbi.engel33@gmail.com>
2022-02-09 10:09:55 -0600

    First commit fixed

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ function main() {
     const sorted = names.sort((a,b) => {
         if (a < b) {
             return -1;
+        } else if (a > b) {
+            return 1;
+        }  else { return 0;
         }
     });
     console.log(sorted);
